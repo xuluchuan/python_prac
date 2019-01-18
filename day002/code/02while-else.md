3ee63959c0249d58e757d75702d37842064737e3
xuluchuan <792375368@qq.com>
2019-01-18 21:23:08 +0800

    day02

---
+++ day002/code/02while-else.md
@@ -0,0 +1,4 @@
+# while-else
+
+- 当while循环正常运行没有break时，不执行else
+- 当while循环break时，执行else
---
+++ day002/code/format_1.py
@@ -0,0 +1,7 @@
+name = input('请输入您的名字：')
+age = int(input('请输入您的年龄：'))
+info = """
+您的姓名是：%s
+您的年龄是：%d
+""" % (name, age)
+print(info)
