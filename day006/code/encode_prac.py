74279fb050726aa8ae8455f7a4fb14065913d196
xuluchuan <792375368@qq.com>
2019-01-27 13:33:08 +0800

    day006

---
+++ day006/code/encode_prac.py
@@ -0,0 +1,10 @@
+s1 = 'alex'
+s11 = s1.encode('utf-8')
+print(s11)
+s2 = '中国'
+print(type(s2))
+s22 = s2.encode('utf-8')
+print(type(s22))
+print(s22)
+s33 = s2.encode('gbk')
+print(s33)
---
+++ day006/code/id_prac.py
@@ -0,0 +1,10 @@
+li = [1, 2, 3]
+li2 = li
+print(id(li))
+print(id(li2))
+print(li is li2)
+s1 = 'abc'
+s2 = 'abc'
+print(id(s1))
+print(id(s2))
+print(s1 is s2)
